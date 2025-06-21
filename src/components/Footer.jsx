79327f45b1f1584a4ad2fe6f659e7b5b5944a10f
prashant9850 <prashantsureshjadhav409@gmail.com>
2025-06-21 22:08:58 +0530

    testing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="h-1/2 bottom-0 w-full bg-red-600">
-      this is the footer secion
+      this is the footer secion fgjnhhuy
     </div>
   );
 }
